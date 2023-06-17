94b72b6408f0ccd03aa67a35bb51328c92dddd95
DaniilVoronkov <79176087+DaniilVoronkov@users.noreply.github.com>
2023-06-16 22:20:57 -0400

    Connected the project to MySQL db. 1) Updated pom.xml. Removed h2 dependency (since we don't need it anymore). 2) Removed unused imports in the main class 3) Commented code in the main class that was filling h2 on the startup (didn't delete for learning purposes)

--- src/main/java/com/createms/learningmicroservices/LearningMicroservicesApplication.java
+++ src/main/java/com/createms/learningmicroservices/LearningMicroservicesApplication.java
@@ -1,14 +1,8 @@
 package com.createms.learningmicroservices;
 
-import com.createms.learningmicroservices.models.enums.TeaType;
-import com.createms.learningmicroservices.models.repositories.TeaRepository;
-import com.createms.learningmicroservices.models.tables.Tea;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
 
@@ -19,18 +13,19 @@ public class LearningMicroservicesApplication {
 	}
 
 
-	//Fast way to declare beans
-	@Configuration
-
-	class LoadDatabase {
-
-		//command line runner means that the method will be invocated when the app is loaded
-		@Bean
-		CommandLineRunner testDB(TeaRepository repository) {
-			return args -> {
-				repository.save(new Tea("Test", TeaType.PACKAGED, 212.1));
-				repository.save(new Tea("Test2", TeaType.LOOSE, 1234521.1));
-			};
-		}
-	}
+	//Fast way to declare beans (commented since we don't need test data to add manually anymore)
+	//now it serves only as an example for learning purposes (as reference)
+//	@Configuration
+//
+//	class LoadDatabase {
+//
+//		//command line runner means that the method will be invocated when the app is loaded
+//		@Bean
+//		CommandLineRunner testDB(TeaRepository repository) {
+//			return args -> {
+//				repository.save(new Tea("Test", TeaType.PACKAGED, 212.1));
+//				repository.save(new Tea("Test2", TeaType.LOOSE, 1234521.1));
+//			};
+//		}
+//	}
 }
