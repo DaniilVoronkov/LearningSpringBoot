14d190beb1fb0381c3fc9a9f7df2c7c01656bc7c
DaniilVoronkov <79176087+DaniilVoronkov@users.noreply.github.com>
2023-06-28 17:36:09 -0400

    Adjusted validation test. Now it no longer uses its own validator.

--- src/test/java/com/createms/learningmicroservices/TeaServiceTest.java
+++ src/test/java/com/createms/learningmicroservices/TeaServiceTest.java
@@ -1,37 +1,26 @@
 package com.createms.learningmicroservices;
 
 import com.createms.learningmicroservices.businesslogic.services.TeaService;
-import com.createms.learningmicroservices.models.abstraction.classesabstraction.ProductDTO;
 import com.createms.learningmicroservices.models.enums.TeaType;
 import com.createms.learningmicroservices.models.repositories.TeaRepository;
 import com.createms.learningmicroservices.models.tables.Tea;
 import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+
 
 import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
@@ -47,8 +36,6 @@ public class TeaServiceTest {
     @InjectMocks
     TeaService teaService;
 
-    private Validator validator;
-
 
     @Test
     void contextLoads() {
@@ -63,11 +50,9 @@ public class TeaServiceTest {
 
     @Test
     @DisplayName("Testing annotation validation")
-    public void saveTeaFail() {
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        validator = factory.getValidator();
+    public void annotationValidation() {
         Tea tea = new Tea("123", TeaType.LOOSE, 123.123);
-        Set<ConstraintViolation<Tea>> violationSet = validator.validate(tea);
+        Set<ConstraintViolation<Tea>> violationSet = teaService.isValid(tea);
         assertFalse(violationSet.isEmpty());
     }
 
@@ -88,6 +73,7 @@ public class TeaServiceTest {
         Optional<Tea> result = Optional.ofNullable(teaService.findById(productId));
         assertTrue(result.isPresent());
         assertEquals(productName, result.get().getName());
+        verify(teaRepository).findById(anyLong());
     }
 
 
