59ed636f79f34a788f82ed2adcb57951741f419a
VoronkovDaniil <kfk.dva@yandex.ru>
2023-07-05 12:52:03 -0400

    Add : add styling to ProductForm.js component

--- frontend/src/components/ProductForm.js
+++ frontend/src/components/ProductForm.js
@@ -1,6 +1,7 @@
 import {Component, useEffect, useState} from "react";
 import "../Styling/FormsStyling.css";
 import {useParams} from "react-router-dom";
+import "../Styling/ValidationStyling.css";
 
 export default function ProductForm() {
 
@@ -43,15 +44,16 @@ export default function ProductForm() {
 
         let isProductNew = id === 'NewProduct';
         const buttonText = isProductNew ? 'Add Product' : 'Edit Product';
-        console.log(JSON.stringify(productValue))
+        console.log(test)
         return (
             <form id="editForm">
                 <div className="form-group">
                     {/* <!-- Product name input --> */}
                     <label htmlFor="productName">Product Name:</label>
+                    {/* Default value property - for initial load!!! */}
                     <input id="productName" type="text" name="productName" placeholder="Name of the product"
                            maxLength="100"
-                           className="form-control" value={isProductNew ? '' : productValue.product.name}/>
+                           className="form-control" defaultValue={isProductNew ? '' : productValue.product.name}/>
                     <label htmlFor="productName" className="errorMessage productNameError"
                            id="productNameError"></label>
                 </div>
@@ -60,17 +62,17 @@ export default function ProductForm() {
                     {/* <!-- Product price input --> */}
                     <label htmlFor="productPrice">Price:</label>
                     <input id="productPrice" type="number" name="productPrice" placeholder="Product price:"
-                           className="form-control" value={isProductNew ? '' : productValue.product.price}/>
+                           className="form-control" defaultValue={isProductNew ? '' : productValue.product.price}/>
                     <label htmlFor="productPrice" className="errorMessage" id="productPriceError"></label>
                 </div>
 
-                {/*<div className="form-group">*/}
-                {/*    /!* <!-- product type select --> *!/*/}
-                {/*    <label htmlFor="productType">Product Type:</label>*/}
-                {/*    <select name="productType" id="productType" className="form-select form-control">*/}
-                {/*        {test.map(type => <option value={type.label}></option>)}*/}
-                {/*    </select>*/}
-                {/*</div>*/}
+                <div className="form-group">
+                    {/* <!-- product type select --> */}
+                    <label htmlFor="productType">Product Type:</label>
+                    <select name="productType" id="productType" className="form-select form-control">
+                        {test.productTypes.map(type => <option key={type} defaultValue={type}>{type}</option>)}
+                    </select>
+                </div>
 
                 <div className="form-group">
                     {/* <!-- Edit button --> */}
