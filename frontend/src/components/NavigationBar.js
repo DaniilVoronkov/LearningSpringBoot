e6dcae00820c7702feb4ed245485bc6e88d0a9fa
VoronkovDaniil <kfk.dva@yandex.ru>
2023-07-09 13:17:05 -0400

    Add <NavigationBar.js> : Add Additional links to the navigation bar

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -5,7 +5,10 @@ export default class NavigationBar extends Component {
 
     render() {
         return <Navbar>
-            <NavLink  href="/Tea/ProductsTable">Tea List</NavLink>
+            <NavLink href="/" className="NavigationLink">Home</NavLink>
+            <NavLink href="#" className="NavigationLink">All Products</NavLink>
+            <NavLink  href="/Tea/ProductsTable" name="Tea" className="NavigationLink">Tea Products</NavLink>
+            <NavLink href="#" className="NavigationLink">Coffee products</NavLink>
         </Navbar>
     }
 }
