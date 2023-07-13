2b7365ff21a9520554a2825feb423f4aa1547410
VoronkovDaniil <kfk.dva@yandex.ru>
2023-07-13 14:29:30 -0400

    Refactor : Removed redundant files (files that were used for rendering before react)

--- Backend/src/main/resources/static/JSScripts/CreateObjectForSending.js
+++
@@ -1,8 +0,0 @@
-//creating an object based on the input fields
-export function createObjectForSending() {
-    return {
-        name: $("#productName").val(),
-        price: $("#productPrice").val(),
-        type: $("#productType option:selected").text()
-    };
-}
--- Backend/src/main/resources/static/JSScripts/DeleteRecord.js
+++
@@ -1,9 +0,0 @@
-export function deleteRecord(productClass, productId) {
-    $.ajax({
-        url: '/' + productClass + '/Delete/' + productId,
-        type: 'DELETE',
-        success: function () {
-            window.location = '/' + productClass + '/ProductsTable'
-        }
-    });
-}
