f8df68c6b85c939936c44f4418a296609b772124
VoronkovDaniil <kfk.dva@yandex.ru>
2023-07-20 22:47:41 -0400

    Refactor : Add additional route to the Router

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -4,13 +4,17 @@ import ProductsTable from './components/ProductsTable'
 import ProductForm from "./components/ProductForm";
 import HomeComponent from "./components/HomeComponent";
 
+
 function App() {
+
   return (
     <div className="App">
       <BrowserRouter>
+
         <Routes>
-          <Route exact path= '/Tea/ProductsTable' element={<ProductsTable/>}/>
-          <Route path='/' element={<HomeComponent/>}/>
+          <Route  path='Tea/ProductsTable' element={<ProductsTable/>}/>
+           <Route path='AllProducts' element={<ProductsTable/>}/>
+           <Route path='/' element={<HomeComponent/>}/>
           <Route path=':ProductClass/EditProductPage/:id' element={<ProductForm/>}/>
         </Routes>
       </BrowserRouter>
