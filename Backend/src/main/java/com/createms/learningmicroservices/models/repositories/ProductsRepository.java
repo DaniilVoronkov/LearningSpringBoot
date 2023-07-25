a7031201dfdd8187a04da5832ca26f538532109d
VoronkovDaniil <kfk.dva@yandex.ru>
2023-07-24 21:31:56 -0400

    Add (ProductRepository.java) : Added projection class + projection interface (commented and serves only as learning example) + add method that allows to find product that contains specific string

--- Backend/src/main/java/com/createms/learningmicroservices/models/repositories/ProductsRepository.java
+++ Backend/src/main/java/com/createms/learningmicroservices/models/repositories/ProductsRepository.java
@@ -2,7 +2,51 @@ package com.createms.learningmicroservices.models.repositories;
 
 import com.createms.learningmicroservices.models.abstraction.classesabstraction.Product;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.util.List;
 
 public interface ProductsRepository <T extends Product> extends CrudRepository<T, Long> {
 
+    //interface projection example
+//    interface ProductNames {
+//        String getName();
+//        Long getId();
+//
+//        default String getData() {
+//            return getName() + " " + getId();
+//        }
+//
+//    }
+
+    @Projection(name = "idPlusName", types = {Product.class})
+    class ProductNameAndId {
+        private final Long id;
+        private final String name;
+
+        public ProductNameAndId(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        //For testing purposes
+        @Override
+        public String toString() {
+            return "ProductNameAndId{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
+
+    //finding product that contains given string and returning list with id + name objects
+    List<ProductNameAndId> findByNameContains(String name);
 }
